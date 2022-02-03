7967cf2adb8ce3ca315da08100d17924bfb9b149
Kevin Tsai <ytsai003@ucr.edu>
2022-02-03 14:24:01 -0800

    Add files via upload

--- main/discMain_M.cpp
+++ main/discMain_M.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : Main.cpp
-// Author      : Wenzhao Sun, Ali Nematbakhsh
+// Author      : Wenzhao Sun, Ali Nematbakhsh, Kevin Tsai
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -29,6 +29,10 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort =
 	}
 }
 //test here
+
+/////////////////////////////////////////////////////////////
+/////////////// Reading configuration file //////////////////
+/////////////////////////////////////////////////////////////
 void initializeSlurmConfig(int argc, char* argv[]) {
 	// read configuration.
 	ConfigParser parser;
@@ -72,6 +76,12 @@ void initializeSlurmConfig(int argc, char* argv[]) {
 	}
 }
 
+
+////////////////////////////////////////////////////
+////////// Calculate division threshold ////////////
+////////////////////////////////////////////////////
+// Used in the apical view model to determine the threshold value for division to occur
+// Called in the current script but the information is not used in the current simulation
 void updateDivThres(double& curDivThred, uint& i, double& curTime,  //Ali
 		double& decayCoeff, double& divThreshold) {
         
@@ -81,6 +91,10 @@ void updateDivThres(double& curDivThred, uint& i, double& curTime,  //Ali
 	curDivThred = divThreshold ;
 }
 
+
+/////////////////////////////////////////////////////
+//////////// Start of the simulation ////////////////
+/////////////////////////////////////////////////////
 int main(int argc, char* argv[]) {
 	// initialize random seed.
 	srand(time(NULL));
@@ -141,6 +155,12 @@ int main(int argc, char* argv[]) {
 	double total_Time = mainPara.totalTimeSteps*mainPara.dt;
 	std::cout<<"mainPara.totalTimeStpes = "<<mainPara.totalTimeSteps<<std::endl;
 	std::cout<<"mainPara.dt = "<<mainPara.dt<<std::endl;
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// These variables are needed for several functions in the current code but is not used. It was from an /
+	// earlier version of cell division algorithm. Do not delete without making changes in other files and //
+	// functions that still take this as an input. //////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////
 	double timeRatio;
 	double timeRatio_Crit_actomyo = 2.0;
 	// std::cout<<"Critical timeRatio for actomyosin strength reduction = "<<timeRatio_Crit_actomyo<<std::endl;
@@ -149,33 +169,81 @@ int main(int argc, char* argv[]) {
 	// std::cout<<"Critical timeRatio for ecm strength reduction = "<<timeRatio_Crit_ECM<<std::endl;
 	double timeRatio_Crit_Division = 0.2;
 	// std::cout<<"Critical timeRatio for growth and cell division = "<<timeRatio_Crit_Division<<std::endl;
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
 	bool reduced_ecm_triggered = false;
+	// Initialization of the true-false marker for the utilization of anisotropic ECM stiffness.
+	
 	bool Division_triggered = false;
-	double volume_Increase_Target_Ratio = 1.41;
+	// Initialization of the true-false marker for the occurrence of cell division.
+	
+	double volume_Increase_Target_Ratio = 1.4;
 	std::cout<<"Target ratio for cell volume increase = "<<volume_Increase_Target_Ratio<<std::endl;
+	// Target cell volume increase during mitotic rounding.
+
 	double volume_Increase_Scale = 1.0;
 	std::cout<<"How fast is the volume increase happening = x"<<volume_Increase_Scale<<" rate of change"<<std::endl;
+	// A scale that determines how fast target cell volume during mitotic rounding will be achieved.
+
 	double postDivision_restorationRateScale = 1.0;//0.5;//0.1;
-	//I changed postDivision_restorationRateScale to 2.0 due to extended simulation steps (1000/0.002), make sure this is changed back
-	//later when using a smaller number of simulation time steps.
 	std::cout<<"How fast is the volume restoration happening post division = x"<<postDivision_restorationRateScale<<" rate of change"<<std::endl;
+	// A scale that determines how fast the divided cell will go back to its original equilibrium cell volume.
+	
 	bool volume_restoration_rate_restore = false;
 
-	double thresholdToIntroduceNewCell = 0.25;//-1.0;
+	double thresholdToIntroduceNewCell = 0.25;//0.5;//1.0;//0.25;//-1.0;
 	std::cout<<"The likelihood (probability) a new cell will be introduced in the same cross section after cell division = "<<thresholdToIntroduceNewCell<<std::endl;
+
+	double mitoticThreshold = 0.95;//0.8973;
+	std::cout<<"mitoticThreshold = "<<mitoticThreshold<<std::endl;
+	// The counter value that needs to be reached for division to occur. The max value of the counter is 1.
 
 	double distFromNucleus_max = 0.0;//4.0;
 	double distFromNucleus_min = 0.3;//-14.0;
-	double distFromNucleus_normalMax = 0.20;//0.355;//-8.0;
-	double distFromNucleus_normalMax_apical = 0.20;//0.295;//7.5;
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	/////// These values determine the portion of cells occupied by the contractile springs /////////
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	double distFromNucleus_normalMax1 = 0.3;//0.275;//0.20;//0.355;//-8.0;
+	double distFromNucleus_normalMax2 = 0.2645;//0.275;//0.20;//0.355;//-8.0;
+	double distFromNucleus_normalMax3 = 0.2615;//0.275;//0.20;//0.355;//-8.0;
+	double distFromNucleus_normalMax_apical1 = 0.2918;//0.175;//0.20;//0.295;//7.5;
+	double distFromNucleus_normalMax_apical2 = 0.2883;//0.175;//0.20;//0.295;//7.5;
+	double distFromNucleus_normalMax_apical3 = 0.2429;//0.175;//0.20;//0.295;//7.5;
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	
 	double percentage_before_timeRatio_Crit_Division_scaling = 4.0; //No longer in use, but don't delete yet since it is still passed into several functions. //Kevin
-	double mitoRndActomyoStrengthScaling = 5.0; //Please consult SceNodes.cu to see what are the scaling applied to non-dividing cells and disc_NXX_X.cfg file to see what is the corresponding default spring constant. //Kevin
+	double mitoRndActomyoStrengthScaling = 7.0; //Please consult SceNodes.cu to see what are the scaling applied to non-dividing cells and disc_NXX_X.cfg file to see what is the corresponding default spring constant. //Kevin
 	std::cout<<"Cell division requires the contractile spring to increase strength by "<<percentage_before_timeRatio_Crit_Division_scaling<<" fold."<<std::endl;
 	std::cout<<"Contractile spring minimum at "<<distFromNucleus_min<<" and maximum at "<<distFromNucleus_max<<" away from the cell center."<<std::endl;
-	std::cout<<"But under non-growth (stationary) circumstances, the basal contractile spring is set at "<<distFromNucleus_normalMax<<"*cellheight away from the cell center."<<std::endl;
-	std::cout<<"But under non-growth (stationary) circumstances, the apical contractile spring is set at "<<distFromNucleus_normalMax_apical<<"*cellheight away from the cell center."<<std::endl;
+	std::cout<<"But under non-growth (stationary) circumstances, the basal contractile spring is set at "<<distFromNucleus_normalMax1<<", "<<distFromNucleus_normalMax2<<", "<<distFromNucleus_normalMax3<<"*cellheight away from the cell center."<<std::endl;
+	std::cout<<"But under non-growth (stationary) circumstances, the apical contractile spring is set at "<<distFromNucleus_normalMax_apical1<<", "<<distFromNucleus_normalMax_apical2<<", "<<distFromNucleus_normalMax_apical3<<"*cellheight away from the cell center."<<std::endl;
 
-	double growthProgressSpeed = 3.84e-7;//3.84e-7;//(0.02)*0.25*(0.001*0.002); // 0.002 is the default time step of the simulation.
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////// Weights applied to the coefficients of the contractile springs ///////////////
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	double contractActomyo_multip_perCell1 = 1.0;
+	double contractActomyo_multip_perCell2 = 1.0;
+	double contractActomyo_multip_perCell3 = 1.0;
+	double contractActomyo_multip_perCell_apical1 = 0.0625;
+	double contractActomyo_multip_perCell_apical2 = 0.0625;
+	double contractActomyo_multip_perCell_apical3 = 0.0625;
+	std::cout<<"Basal_left_actomyo_weight = "<<contractActomyo_multip_perCell1<<std::endl;
+	std::cout<<"Basal_mid_actomyo_weight = "<<contractActomyo_multip_perCell2<<std::endl;
+	std::cout<<"Basal_right_actomyo_weight = "<<contractActomyo_multip_perCell3<<std::endl;
+	std::cout<<"Apical_left_actomyo_weight = "<<contractActomyo_multip_perCell_apical1<<std::endl;
+	std::cout<<"Apical_mid_actomyo_weight = "<<contractActomyo_multip_perCell_apical2<<std::endl;
+	std::cout<<"Apical_right_actomyo_weight = "<<contractActomyo_multip_perCell_apical3<<std::endl;
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+
+	double growthProgressSpeed = 0.5*3.84e-7;//3.84e-7;//(0.02)*0.25*(0.001*0.002); 
+	// 0.002 is the default simulation time step size. The value above indicate how much progress is gained per time step.
 	std::cout<<"Growth speed for cell cycle per time step is = "<<growthProgressSpeed<<std::endl;
 
 	int maxApicalBasalNodeNum = 18;//9999;
@@ -185,20 +253,10 @@ int main(int argc, char* argv[]) {
 
 	double maxLengthToAddMemNodes = 0.195;//0.26;
 	std::cout<<"Max length for each edge to qualify for new node additions = "<<maxLengthToAddMemNodes<<std::endl;
+	// This is restricted to apical and basal side of the cell only, due to the constraint of the number of nodes placed laterally.
 
 	std::vector<int> cycle_vec;
 	cycle_vec.push_back(-1);
-	// uint maxNumCycle = 10;
-
-	// for (int k = 0; k < maxNumCycle; k++){
-	// 	cycle_vec.push_back(k);
-	// }
-	// if (maxNumCycle == 0){
-	// 	cycle_vec.push_back(-1);
-	// }
-	// cycle_vec.push_back(0);
-	// cycle_vec.push_back(1);
-	// cycle_vec.push_back(2);
 	int cycle;
 
 	double fixed_dt = mainPara.dt;
@@ -210,84 +268,36 @@ int main(int argc, char* argv[]) {
 			// current_Time = i*mainPara.dt + mainPara.InitTimeStage;
 			current_Time = i*fixed_dt + mainPara.InitTimeStage;
 			timeRatio = current_Time/total_Time;
-			// if (timeRatio > timeRatio_Crit_actomyo && reduced_actomyo_triggered == false){
-			// 	std::cout<<"Reduced actomyosin strength in the tissue midsection triggered."<<std::endl;
-			// 	std::cout<<"Current timeRatio for reduced actomyosin ="<<timeRatio<<std::endl;
-			// 	reduced_actomyo_triggered = true;
-			// }
-			// if (timeRatio > timeRatio_Crit_ECM && reduced_ecm_triggered == false){
-			// 	std::cout<<"Reduced ECM strength in the tissue midsection triggered."<<std::endl;
-			// 	std::cout<<"Current timeRatio for reduced ecm ="<<timeRatio<<std::endl;
-			// 	reduced_ecm_triggered = true;
-			// }
-			// if (timeRatio == timeRatio_Crit_Division && Division_triggered == false){
-			// 	std::cout<<"Division triggered in discMain.cpp"<<std::endl;
-			// 	std::cout<<"Current timeRatio for division ="<<timeRatio<<std::endl;
-			// 	Division_triggered = true;
-			// 	// mainPara.dt = 1e-6;
-			// 	volume_restoration_rate_restore = false;
-			// }
-			// if (timeRatio > (timeRatio_Crit_Division+0.05)){
-			// 	mainPara.dt = fixed_dt;
-			// 	// if (volume_restoration_rate_restore == false){
-			// 	// 	std::cout<<"Volume restoration rate restored to normal speed"<<std::endl;
-			// 	// }
-			// 	// volume_restoration_rate_restore = true;
-			// 	// postDivision_restorationRateScale = 0.2;
-			// 	// break;
-			// }
+			
 			// this if is just for output data// 
 			if (i % mainPara.aniAuxVar == 0) {
-				// if (i == 0 && cycle != 0){
+				double curTime=i*mainPara.dt + mainPara.InitTimeStage;  //Ali - Abu
+				updateDivThres(curDivThred, i, curTime, decayCoeff,divThreshold);
 
-				// }
-				// else{
-					double curTime=i*mainPara.dt + mainPara.InitTimeStage;  //Ali - Abu
-					updateDivThres(curDivThred, i, curTime, decayCoeff,divThreshold);
-
-					// std::cout << "substep 1" << std::flush;
-
-					CellsStatsData polyData = simuDomain.outputPolyCountData();  //Ali comment
-					std::cout<<"survived outputPolyCountData()?"<<std::endl;
-					singleCellData=simuDomain.OutputStressStrain() ;              
-					singleCellData.printStressStrainToFile(stressStrainFileName,curTime) ;
-								
-
-					// std::cout << "substep 2 " << std::endl;
-					// prints brief polygon counting statistics to file
-					polyData.printPolyCountToFile(polyStatFileName, curDivThred);
-					// prints detailed individual cell statistics to file
-					polyData.printDetailStatsToFile(detailStatFileNameBase, aniFrame);
-					// prints the animation frames to file. They can be open by Paraview
-
-					
-					//if (i != 0) {
-						//simuDomain.processT1Info(maxStepTraceBack, polyData);
-					//}
-
-					// std::cout << "substep 3 " << std::endl;
-					//simuDomain.outputVtkFilesWithCri_M(mainPara.animationNameBase,
-					//		aniFrame, mainPara.aniCri);
-					//simuDomain.outputVtkColorByCell_T1(mainPara.animationNameBase,
-					//		aniFrame, mainPara.aniCri);
-					simuDomain.outputVtkColorByCell_polySide(mainPara.animationNameBase,
-							aniFrame, mainPara.aniCri);
-					// std::cout << "in ani step " << aniFrame << std::endl;
-					// std::cout << "substep 4 " << std::endl;
-					simuDomain.outputResumeData(aniFrame) ; 
-					aniFrame++;
-				// }
+				CellsStatsData polyData = simuDomain.outputPolyCountData();  //Ali comment
+				std::cout<<"survived outputPolyCountData()?"<<std::endl;
+				singleCellData=simuDomain.OutputStressStrain() ;              
+				singleCellData.printStressStrainToFile(stressStrainFileName,curTime) ;
+							
+				// prints brief polygon counting statistics to file
+				polyData.printPolyCountToFile(polyStatFileName, curDivThred);
+				// prints detailed individual cell statistics to file
+				polyData.printDetailStatsToFile(detailStatFileNameBase, aniFrame);
+				// prints the animation frames to file. They can be open by Paraview
+				simuDomain.outputVtkColorByCell_polySide(mainPara.animationNameBase,
+						aniFrame, mainPara.aniCri);
+				
+				simuDomain.outputResumeData(aniFrame) ; 
+				aniFrame++;
 			}
 			simuDomain.runAllLogic_M(mainPara.dt,mainPara.Damp_Coef,mainPara.InitTimeStage,
 									timeRatio, timeRatio_Crit_actomyo, timeRatio_Crit_ECM, timeRatio_Crit_Division,
 										volume_Increase_Target_Ratio, volume_Increase_Scale, postDivision_restorationRateScale, cycle,
-										distFromNucleus_max, distFromNucleus_min, distFromNucleus_normalMax, distFromNucleus_normalMax_apical,
-										percentage_before_timeRatio_Crit_Division_scaling, growthProgressSpeed, maxApicalBasalNodeNum, maxLengthToAddMemNodes,mitoRndActomyoStrengthScaling, thresholdToIntroduceNewCell);  //Ali //Kevin
-			// simuDomain.runAllLogic_M(mainPara.dt,mainPara.Damp_Coef,mainPara.InitTimeStage,
-			// 						timeRatio, timeRatio_Crit_actomyo, timeRatio_Crit_ECM, timeRatio_Crit_Division,
-			// 							volume_Increase_Target_Ratio, volume_Increase_Scale, postDivision_restorationRateScale, cycle,
-			// 							distFromNucleus_max, distFromNucleus_min, distFromNucleus_normalMax, distFromNucleus_normalMax_apical,
-			// 							percentage_before_timeRatio_Crit_Division_scaling, growthProgressSpeed, maxApicalBasalNodeNum, minApicalBasalNodeNum, maxLengthToAddMemNodes);  //Ali //Kevin
+										distFromNucleus_max, distFromNucleus_min, distFromNucleus_normalMax1, distFromNucleus_normalMax2, distFromNucleus_normalMax3,
+										distFromNucleus_normalMax_apical1, distFromNucleus_normalMax_apical2,distFromNucleus_normalMax_apical3,
+										percentage_before_timeRatio_Crit_Division_scaling, growthProgressSpeed, maxApicalBasalNodeNum, maxLengthToAddMemNodes,mitoRndActomyoStrengthScaling, thresholdToIntroduceNewCell,
+										contractActomyo_multip_perCell1, contractActomyo_multip_perCell2, contractActomyo_multip_perCell3,
+										contractActomyo_multip_perCell_apical1, contractActomyo_multip_perCell_apical2, contractActomyo_multip_perCell_apical3, mitoticThreshold);  //Ali //Kevin
 		}
 	}
 
